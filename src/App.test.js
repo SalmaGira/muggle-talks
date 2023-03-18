11fb76ff40332fd141e827bdc0f4eb24ffd0c580
SalmaGira <salmagira16@gmail.com>
2023-03-18 22:20:44 +0200

    test: adding api testing with nock

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
 import { render, screen, waitFor } from '@testing-library/react'
 import App from './App'
+import nock from 'nock'
+
+const url = 'https://jsonplaceholder.typicode.com'
 
 describe('expectedData', () => {
   it('checks if returned data from API rendered into component', async () => {
+    nock(`${url}/posts`)
+      .defaultReplyHeaders({
+        'access-control-allow-origin': '*',
+      })
+      .get('/users/1')
+      .reply(200, {
+        id: 1,
+        title:
+          'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+      })
+
     render(<App />)
 
     await waitFor(() => {
